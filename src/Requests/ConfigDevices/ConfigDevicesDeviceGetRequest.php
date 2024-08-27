ae0c5a03b5ba20ee759efdd07288d09bfb26b203
sentosango <sentosango@gmail.com>
2024-08-28 00:54:33 +0300

    Remove DTOs

--- src/Requests/ConfigDevices/ConfigDevicesDeviceGetRequest.php
+++ src/Requests/ConfigDevices/ConfigDevicesDeviceGetRequest.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace PhpClient\Syncthing\Requests\ConfigDevices;
 
-use PhpClient\Syncthing\Dto\Device;
-use PhpClient\Syncthing\Exceptions\SyncthingException;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
-use Saloon\Http\Response;
 
 /**
  * Returns the device for the given ID.
@@ -27,13 +24,5 @@ final class ConfigDevicesDeviceGetRequest extends Request
     public function resolveEndpoint(): string
     {
         return "rest/config/devices/$this->device";
-    }
-
-    /**
-     * @throws SyncthingException
-     */
-    public function createDtoFromResponse(Response $response): Device
-    {
-        return Device::fromArray(array:$response->array());
     }
 }
